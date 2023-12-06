532f77107ab4f245145e8016edfbe80947f63a4a
Zhaziraak <zhaziraakaeva@gmail.com>
2023-12-06 19:57:59 +0600

    added random

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,44 @@
+import java.util.Random;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println(dannye(21, -10));
-        System.out.println(dannye(15, -18));
-        System.out.println(dannye(50, 2));
-        System.out.println(dannye(39, 31));
-        System.out.println(dannye(5, 30));
+        System.out.println(ABC(10, -2));
+        System.out.println(ABC(22, -5));
+        System.out.println(ABC(19, -16));
+        System.out.println(ABC(37, 21));
+        System.out.println(ABC(45, 30));
+        System.out.println();
+
+
+        System.out.println(ABC(generateRandomAge(), 15));
+        System.out.println(ABC(generateRandomAge(), -50));
+        System.out.println(ABC(generateRandomAge(), 25));
+        System.out.println(ABC(generateRandomAge(), 3));
+        System.out.println(ABC(generateRandomAge(), -10));
+
     }
 
-    public static String dannye (int ageOfHuman, double temperature) {
-        if (ageOfHuman > 20 && ageOfHuman < 45 && temperature > -20 && temperature < 30)
+    public static String ABC (int ageOfHuman, int streetTemperature) {
+        if (ageOfHuman > 20 && ageOfHuman < 45 && streetTemperature > -20 &&
+                streetTemperature < 30)
             return "Можно идти гулять";
 
-        else if (ageOfHuman < 20 && temperature > 0 && temperature < 28)
+        else if (ageOfHuman < 20 && streetTemperature > 0 &&
+                streetTemperature < 28)
             return "Можно идти гулять";
 
-        else if (ageOfHuman > 45 && temperature > -10 && temperature < 25)
+        else if (ageOfHuman > 45 && streetTemperature > -10 && streetTemperature < 25)
             return "Можно идти гулять";
 
         else
             return "Оставайтесь дома";
     }
-    
+
+    public static int generateRandomAge(){
+        Random randAge = new Random();
+        int limit = 100;
+        return randAge.nextInt(limit);
+
+    }
 }
